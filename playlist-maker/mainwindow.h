9a500eac27dbb141367b217f665849cdc5e1027b
alypko <artem2310@hotmail.com>
2021-11-21 17:10:14 +0100

    add api wrapper, create plylist

---
+++ playlist-maker/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <QOAuth2AuthorizationCodeFlow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+
+    void granted();
+    void authStatusChanged (QAbstractOAuth::Status status);
+    void on_actionGrant_triggered();
+    void on_actionGet_User_Information_triggered();
+    void on_actionGet_Playlists_triggered();
+    void on_actionCreate_Playlist_triggered();
+
+private:
+    void setParams();
+    Ui::MainWindow *ui;
+    QOAuth2AuthorizationCodeFlow spotify;
+    bool isGranted;
+    QString userName;
+};
+
+#endif // MAINWINDOW_H
