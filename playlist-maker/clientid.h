578be4252c8b46b8e2d68181c76d24ff98974f38
alypko <artem2310@hotmail.com>
2021-11-21 17:01:33 +0100

    Add Api wrapper, create playlist

---
+++ playlist-maker/clientid.h
@@ -0,0 +1,8 @@
+#ifndef CLIENTID_H
+#define CLIENTID_H
+#include <QString>
+
+const QString clientId = "4be51445e09b4857a1e7938a4cea720a";
+const QString clientSecret = "3c0840cc3c1f4fd0bc0903f13d626e30";
+
+#endif // CLIENTID_H
